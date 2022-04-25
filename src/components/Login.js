12e44aff9ea6b08201b3d2c1e855e0c2f6e16c4d
Renzo Marangon <renzomarangon@gmail.com>
2022-04-24 21:50:25 -0300

    Firebase | Auth + Storage

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,14 +1,27 @@
 import React,{ useState, useContext, useEffect } from 'react'
 import { Button } from '@mui/material'
 import db from '../firebase'
-import { GoogleAuthProvider, getAuth, signInWithPopup  } from "firebase/auth";
+import { GoogleAuthProvider, getAuth, signInWithPopup, onAuthStateChanged, signOut, signInWithRedirect } from "firebase/auth";
 import LoginContext from '../context/LoginContext';
+import { addDoc, collection, setDoc, doc, getDocs, getDoc } from "firebase/firestore";
 
 const Login = () => {
 
+
+    const [ userID, setUserID ] = useState('');
+
+    const [ buyerData, setBuyerData ] = useState({
+      mail:'',
+      name:'',
+      image:'',
+    });
+
     const { userProvider, setUserProvider } = useContext(LoginContext);
 
-
+    useEffect(()=>{
+      // isUserRegister('asd') ? console.log('si esta') : console.log('no esta')
+      isUserRegister('YlDZ6J22bSbSEcZJP2x3fCzC8pr1')
+    },[buyerData])
 
     const googleAuth = () => {
         const provider = new GoogleAuthProvider();
@@ -19,21 +32,73 @@ const Login = () => {
           
           const { email, displayName, photoURL } = result.user
 
+          // const credential = GoogleAuthProvider.credentialFromResult(result);
+          // const token = credential.accessToken;
+          // const user = result.user;
+
+          setBuyerData({
+            mail:email,
+            name:displayName,
+            image:photoURL
+          })
+
+
+
+
         })
     }
 
-    
-
-    const moxtrar = () => {
-      console.log(userProvider)
+    const mostrarUsuario = () => {
+      const auth = getAuth();
+      onAuthStateChanged(auth, (user) => {
+        if (user) {
+             setUserID(user.uid);
+        } else {
+        }
+      });
     }
 
+    const salirDeLaCuenta = () =>{
+      const auth = getAuth();
+      {
+        mostrarUsuario && (
+          signOut(auth).then(() => {
+            console.log('Se fue de la cuenta')
+          }).catch((error) => {
+            // An error happened.
+          })
+        )
+      }
+    }
+
+    
+    const registerUsers = async(id,object) =>{
+
+      const fireStore = collection(db, "users");
+      const addToFirestore = await setDoc(doc(db,"users",id),
+      object
+      )}
+
+    const mostrar = () =>{
+      setBuyerData({name:'negro'});
+    }
+
+    const isUserRegister = async(id) => {
+
+      let isIn = false;
+      const docRef = doc(db, "users", id);
+      const docSnapShot = await getDoc(docRef);
+      docSnapShot!='undefined' ? (isIn = true) : (isIn = false);
+    
+      console.log(docSnapShot)
+    }
 
   return (
     <>
       <div>Login</div>
       <Button onClick={ googleAuth }>Acceder</Button>
-      <Button onClick={ moxtrar }>Acceder</Button>
+      <Button onClick={ mostrar }>Mostrar</Button>
+      <Button onClick={ salirDeLaCuenta }>Salir</Button>
     </>
     
 
